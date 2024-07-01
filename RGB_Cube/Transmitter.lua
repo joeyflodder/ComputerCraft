537a7e9fb998d40a4d3956f16ae87f2368c0944f
joeyflodder <76872623+joeyflodder@users.noreply.github.com>
2024-07-01 22:53:24 +0200

    Update RGB_Cube/Transmitter.lua

--- RGB_Cube/Transmitter.lua
+++ RGB_Cube/Transmitter.lua
@@ -1,13 +1,12 @@
 modem = peripheral.wrap("top")
-colorCode = {14,1,4,13,9,11,10,6}
-while(true)
-do
-    while(redstone.getInput("Front") == false)
-    do
-        for i= 1, 8
-        do
-            modem.transmit(i, i, colorCode[i])
-            sleep(0.05)
+colorCode = {14,1,4,13,9,11,10,6,14,1,4,13,9,11,10,6}
+while(true) do
+    while(redstone.getInput("Front") == false) do
+        for x=1, 8 do
+            for i=1, 8 do
+                modem.transmit(i, i, colorCode[i+x])
+                sleep(0.05)
+            end
         end
     end
     sleep(1)
