2cf01c518d969f0c26e85bbfda68263f2242988c
joeyflodder <76872623+joeyflodder@users.noreply.github.com>
2024-07-01 22:55:00 +0200

    Update RGB_Cube/Transmitter.lua

--- RGB_Cube/Transmitter.lua
+++ RGB_Cube/Transmitter.lua
@@ -5,7 +5,7 @@ while(true) do
         for x=1, 8 do
             for i=1, 8 do
                 modem.transmit(i, i, colorCode[i+x])
-                sleep(0.05)
+                sleep(0.1)
             end
         end
     end
