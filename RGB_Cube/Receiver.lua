5722b95b9e8457ca8402dfe10682ea03858dec6d
joeyflodder <76872623+joeyflodder@users.noreply.github.com>
2024-07-01 19:06:50 +0200

    Update RGB_Cube/Receiver.lua

--- RGB_Cube/Receiver.lua
+++ RGB_Cube/Receiver.lua
@@ -1,4 +1,8 @@
 modem = peripheral.wrap("top")
 modem.open(0)
-event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-if type(message) == "number" then redstone.setAnalogOutput("bottom", message) end
+while true do
+    event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    if type(message) == "number" then 
+        redstone.setAnalogOutput("bottom", message) 
+    end
+end
